e6bdfef9ba562d0d2a702a00a6921333462fe5ea
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2023-06-21 21:10:23 +0300

    Completed playRound function

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+// Getting a random choice for the computers turn.
 function getComputerChoice() {
-
   let compChoice = ["rock", "paper", "scissors"];
   return compChoice[Math.floor(Math.random() * compChoice.length)];
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Please choose between: \"Rock \"Paper \"Scissors").toUpperCase();
-console.log("Computer gets: " + " " + getComputerChoice());
-
-
+// Starts the round and checking each statement to define a winner.
 function playRound(playerSelection, computerSelection) {
+  if ((playerSelection === "rock" && computerSelection === "rock") ||
+  (playerSelection === "paper" && computerSelection === "paper") ||
+  (playerSelection === "scissors" && computerSelection === "scissors")) {
+    return "It's a Tie!";
 
-  if (playerSelection === "rock" && computerSelection === 0) {
-    console.log("Its a tie!");
+  } else if ((playerSelection === "rock" && computerSelection === "paper") ||
+  (playerSelection === "paper" && computerSelection === "scissors") ||
+  (playerSelection === "scissors" && computerSelection === "rock")) {
+    return "Player loses, Computer wins!";
+
+  } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+  (playerSelection === "paper" && computerSelection === "rock") ||
+  (playerSelection === "scissors" && computerSelection === "paper")) {
+    return "Player wins, Computer loses!";
   }
 }
 
+// Prompts user to choose an option.
+const playerSelection = prompt("Please choose between: \"Rock \"Paper \"Scissors").toUpperCase().toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log("Computer gets: " + " " + computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
