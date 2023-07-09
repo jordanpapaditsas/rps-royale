0df2426d007328aaf555c876bcfe17babcd67631
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2023-07-09 19:23:56 +0300

    Finished UI

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 // Initializing variables
 const computerSelection = getComputerChoice();
-
-let scoreCounter = 0;
+let roundCounter = 1;
 let playerScore = 0;
 let computerScore = 0;
-
-
+let tieScore = 0;
+let roundResults = '';
 
 /**
  *  Selection of elements
  */
-
-
-
-
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const resetBtn = document.querySelector('#resetBtn');
+let roundResultsText = document.querySelector('#roundResults');
+let roundCounterText = document.querySelector('#counter');
+let playerScoreText = document.querySelector('#player');
+let computerScoreText = document.querySelector('#computer');
+let tieScoreText = document.querySelector('#tie');
 
 /**
  *  Event listeners
  */
-
-
-
+rockBtn.addEventListener('click', playRound);
+paperBtn.addEventListener('click', playRound);
+scissorsBtn.addEventListener('click', playRound);
+resetBtn.addEventListener('click', resetGame);
 
 // Getting a random choice for the computers turn.
 function getComputerChoice() {
@@ -28,56 +33,75 @@ function getComputerChoice() {
   return compChoice[Math.floor(Math.random() * compChoice.length)];
 }
 
-// Receives input from the user, comparing the value with the computers value, and determines the winner.
+// Starts the round, gets the players choice, and a random computers choice, and declares the winner of the round.
 function playRound(event) {
   const playerSelection = event.target.id;
-
   const computerSelection = getComputerChoice();
   
   if ((playerSelection === 'rock' && computerSelection === "rock") ||
   (playerSelection === 'paper' && computerSelection === "paper") ||
   (playerSelection === 'scissors' && computerSelection === "scissors")) {
-    fightRoundResults.textContent = 'It\'s a tie';
-    checkRoundResults();
+    roundResultsText.innerHTML = `You choose: ${playerSelection} <br> 
+    Computer chooses: ${computerSelection} <br> Its a tie!`;
+    tieScore++;
+    tieScoreText.textContent = 'Tie: ' + tieScore;
 
   } else if ((playerSelection === 'rock' && computerSelection === "paper") ||
   (playerSelection === 'paper' && computerSelection === "scissors") ||
   (playerSelection === 'scissors' && computerSelection === "rock")) {
-    fightRoundResults.textContent = 'Computer wins the round!';
-    checkRoundResults();
+    roundResultsText.innerHTML = `You choose: ${playerSelection} <br> 
+    Computer chooses: ${computerSelection} <br> Computer wins the round!`;
+    computerScore++;
+    computerScoreText.textContent = 'Computer: ' + computerScore;
 
   } else if ((playerSelection === 'rock' && computerSelection === "scissors") ||
   (playerSelection === 'paper' && computerSelection === "rock") ||
   (playerSelection === 'scissors' && computerSelection === "paper")) {
-    fightRoundResults.textContent = 'Player wins the round!';
-    checkRoundResults();
-  }
+    roundResultsText.innerHTML = `You choose: ${playerSelection} <br> 
+    Computer chooses: ${computerSelection} <br> You win the round!`;
+    playerScore++;
+    playerScoreText.textContent = 'Player: ' + playerScore;
+  };
+
+  roundCounter++;
+  roundCounterText.textContent = 'Round: ' + roundCounter;
+  if (playerScore === 5 || computerScore === 5 || tieScore === 5) {
+    declareWinner();
+  };
 };
 
-
-function checkRoundResults() {
-  if (fightRoundResults.textContent === 'It\'s a tie') {
-      scoreCounter++;
-      playerScore += 0;
-      computerScore += 0;
-  }
-  if (fightRoundResults.textContent === 'Computer wins the round!') {
-      scoreCounter++;
-      computerScore++;
-  }
-  if (fightRoundResults.textContent === 'Player wins the round!') {
-      scoreCounter++
-      playerScore++;
-  }
-}
-
+/**
+ *  Checks who wins the game, and provides the decision message.
+ */
+function declareWinner() {
+  if (playerScore === 5) {
+    roundResultsText.innerHTML = 'You won the game!';
+  };
+  if (computerScore === 5) {
+    roundResultsText.innerHTML = 'Computer won the game!';
+    
+  };
+  if (tieScore === 5) {
+    roundResultsText.innerHTML = 'Its a draw!';
+  };
+};
 
 // Resets the game
+function resetGame() {
+  roundCounter = 1;
+  playerScore = 0;
+  computerScore = 0;
+  tieScore = 0;
 
+  roundCounterText.textContent = 'Round: ' + roundCounter;
+  playerScoreText.textContent = 'Player: ' + playerScore;
+  computerScoreText.textContent = 'Computer: ' + computerScore;
+  tieScoreText.textContent = 'Tie: ' + tieScore;
+  roundResultsText.innerHTML = roundResults;
+};
 
-
-
-
+// Style for the text results
+roundResultsText.style.fontSize = '2rem';
 
 
 
